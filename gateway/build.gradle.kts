b57a0b5ffd5be6ba4b59c2dcaa884931c1a2d31e
YatoroG <endlessly.space.com@gmail.com>
2026-07-03 11:31:29 +0300

    Fix build.gradle.kts and docker-compose

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -4,9 +4,15 @@ plugins {
     id("io.spring.dependency-management")
 }
 
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.0")
+    }
+}
+
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     compileOnly("org.projectlombok:lombok:1.18.30")
     annotationProcessor("org.projectlombok:lombok:1.18.30")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.8.5")
 }
